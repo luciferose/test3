0d673b224f691f0554e6d46581fc647801918b8d
Administrator <20139816@qq.com>
2018-11-13 10:26:55 +0800

    add queuetest

--- queuetest2.py
+++ queuetest2.py
@@ -7,12 +7,12 @@ import _thread as thread,queue,time
 safeprint=thread.allocate_lock()
 dataqueue=queue.Queue()
 
-def producer(idnum):
+def producer(idnum,dataqueue):
     for msgnum in range(nummessages):
         time.sleep(idnum)
         dataqueue.put('[producer id=%d count=%d]' % (idnum,msgnum))
 
-def consumer(idnum):
+def consumer(idnum,dataqueue):
     while True:
         time.sleep(0.1)
         try:
@@ -25,8 +25,8 @@ def consumer(idnum):
 
 if __name__=='__main__':
     for i in range(numconsumers):
-        thread.start_new_thread(consumer,(i,))
+        thread.start_new_thread(consumer,(i,dataqueue))
     for i in range(numproducers):
-        thread.start_new_thread(producer,(i,))
-    time.sleep(((numproducers-1)*nummessages)+1)
+        thread.start_new_thread(producer,(i,dataqueue))
+    time.sleep(((numproducers - 1) * nummessages) + 1)
     print('main thread exit.')
